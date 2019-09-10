d4d3d2f66b969923a2ffb8eebd192f9fe3f65b3f
rrmaje <rafal_majewski@instance-1.europe-west4-a.c.ovonpar-system.internal>
2019-09-10 12:08:11 +0000

    Run container in background

--- applications/system/build.sh
+++ applications/system/build.sh
@@ -1,1 +1,1 @@
-docker run --rm --network=tickernet -p4000:4000 -p5000:5000 -p5001:5001 -p6000:6000 -p6001:6001 --name parity-system ovonpar-system/parity-system
+docker run --rm -d --network=tickernet -p4000:4000 -p5000:5000 -p5001:5001 -p6000:6000 -p6001:6001 --name parity-system rrmaje/parity-system
